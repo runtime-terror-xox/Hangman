23786fb3c8407862d929b28f58e557a474567d7f
ahmad-swedani <swedani390@gmail.com>
2020-09-13 17:57:03 +0300

    add a window for let user pick a specific category

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -19,3 +19,21 @@ category_window = Tk()
 category_window.geometry('400x200+761+250')
 category_window.configure(bg="#fff")
 category_window.title('Hangman')
+
+"""
+function to check what is the category and call a new function to open new window (game window) and destroy category window
+"""
+def what_is_category():
+     print(box_value.get())
+     if box_value.get():
+        global category
+        category=box_value.get()
+        game_window()
+
+
+box_value=StringVar()
+coltbox = ttk.Combobox(category_window,textvariable=box_value)
+coltbox["values"] = ["color", "name","country"]
+coltbox.pack()
+bbbb=Button(category_window,text="start\ngame", command=what_is_category, width=20)
+bbbb.pack()
