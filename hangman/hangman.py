2dd39a6490d6e3fb407a71d2728245fbc9869a96
ahmad-swedani <swedani390@gmail.com>
2020-09-13 17:56:57 +0300

    add a window for let user pick a specific category

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -10,3 +10,12 @@ from data_from_faker import *
 alphabet1=[ 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p','a']
 alphabet2=[ 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l','z']
 alphabet3=[ 'x', 'c', 'v', 'b', 'n', 'm']
+
+
+"""
+Category window
+"""
+category_window = Tk()
+category_window.geometry('400x200+761+250')
+category_window.configure(bg="#fff")
+category_window.title('Hangman')
