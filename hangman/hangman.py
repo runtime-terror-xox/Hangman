45b77e276258f201ae1d3a0481de264eefcfa1de
ahmad-swedani <swedani390@gmail.com>
2020-09-16 10:57:24 +0300

    fix some functionality

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -12,19 +12,26 @@ alphabet1=[ 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p','a']
 alphabet2=[ 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l','z']
 alphabet3=[ 'x', 'c', 'v', 'b', 'n', 'm']
 
-
-
-class Game():
-    category=''
-    numberOfGuesses=0
-    def __init__(self):
-        pass
-    
+"""
+function to run the game on new window
+"""
+def game_window():
+    category_window.destroy()
+    global category
+    """
+    Game window
+    """
+    window = Tk()
+    window.geometry('900x610+400+100')
+    window.configure(bg="#fff")
+    window.title('Hangman_Game')
+    photos = [PhotoImage(file='hm01.png'), PhotoImage(file='hm02.png'), PhotoImage(file='hm03.png'),PhotoImage(file='hm06.png'),PhotoImage(file='hm07.png'), PhotoImage(file='hm08.png')]
+    label_word = StringVar()
     """
     function to get random name by the category that user choice
     """
     def random_word():
-        # global category
+        global category
         min, max = 0, 9
         l_idx = random.randint(min, max)
         if category == 'country':
@@ -51,7 +58,6 @@ class Game():
             word_list = faker_language_data()
             random_word = word_list[l_idx]
             return random_word.lower()
-
     """
     function to start the game and count number Of try to Guess
     """
@@ -62,11 +68,10 @@ class Game():
         global word_for_guess
         numberOfGuesses = 0
         if True:
-            word_for_guess=Game.random_word()
+            word_for_guess=random_word()
             the_word_withSpaces = " ".join(word_for_guess)
             print(the_word_withSpaces)
             label_word.set(" ".join("_" * len(word_for_guess)))
-
     """
     function to get the user hint or defintion about the word that must be guessed
     """
@@ -94,7 +99,7 @@ class Game():
     function to check the letter from the user and count the times of try to guess and do some action based on his input
     """
     def if_guess(letter):
-        # global numberOfGuesses
+        global numberOfGuesses
         if numberOfGuesses>=6:
             pass
         try:
@@ -109,37 +114,26 @@ class Game():
                         if label_word.get() == the_word_withSpaces:
                             messagebox.showinfo('Hangman', 'Great you guessed it')
                             global newgame_btn1
-                            newgame_btn1=Button(window, text='New Game', command=lambda: Game.if_user_want_to_play(), font=('Helvetica 18'),width=13, height=2, bg='#263d42', fg="white", bd=1, activebackground="#3e646c",activeforeground="pink").place(relx=0.8, rely = 0.870, anchor=CENTER)
+                            newgame_btn1=Button(window, text='New Game', command=lambda: (if_user_want_to_play()), font=('Helvetica 18'),
+                                                width=13, height=2, bg='#263d42', fg="white", bd=1, activebackground="#3e646c",
+                                                activeforeground="pink").place(relx=0.8, rely = 0.870, anchor=CENTER)
                 else:
                     numberOfGuesses += 1
                     img_label.config(image=photos[numberOfGuesses])
                     img_label.config(bg="#fff")
                     if numberOfGuesses == 5:
+
                         messagebox.showwarning('Hangman', 'Game Over')
                         global newgame_btn2
-                        newgame_btn2=Button(window, text='New Game', command=lambda: Game.if_user_want_to_play(), font=('Helvetica 18'),width=13, height=2, bg='#263d42', fg="white", bd=1, activebackground="#3e646c",activeforeground="pink").place(relx=0.8, rely = 0.870, anchor=CENTER)
+                        newgame_btn2=Button(window, text='New Game', command=lambda: (if_user_want_to_play()), font=('Helvetica 18'),
+                                            width=13, height=2, bg='#263d42', fg="white", bd=1, activebackground="#3e646c",
+                                            activeforeground="pink").place(relx=0.8, rely = 0.870, anchor=CENTER)
         except IndexError as inderr:
             print('in indexerr')
         except:
             print("no")
-            Game.if_guess(get_voice_val())
+            if_guess(get_voice_val())
 
-
-"""
-function to run the game on new window
-"""
-def game_window():
-    category_window.destroy()
-    # global category
-    """
-    Game window
-    """
-    window = Tk()
-    window.geometry('900x610+400+100')
-    window.configure(bg="#fff")
-    window.title('Hangman_Game')
-    photos = [PhotoImage(file='hm01.png'), PhotoImage(file='hm02.png'), PhotoImage(file='hm03.png'),PhotoImage(file='hm06.png'),PhotoImage(file='hm07.png'), PhotoImage(file='hm08.png')]
-    label_word = StringVar()
     img_label = Label(window)
     img_label.grid(row=1, column=3, columnspan=3)
     img_label.config(image=photos[0])
@@ -149,28 +143,28 @@ def game_window():
     for i in range(2):
         Label(window, textvariable='', font=('Helvetica 18'), width=3, height=3, bg="#fff").grid(row=3 + n // 13,column=n % 13)
     for i in alphabet1:
-        Button(window, text=i, command=lambda i=i: Game.if_guess(i), font=('Helvetica 18'), width=5, height=3, bg="#263d42",fg="white", activebackground="#3e646c", activeforeground="pink").grid(row=3 + n // 13,column=n % 13)
+        Button(window, text=i, command=lambda i=i: if_guess(i), font=('Helvetica 18'), width=5, height=3, bg="#263d42",fg="white", activebackground="#3e646c", activeforeground="pink").grid(row=3 + n // 13,column=n % 13)
         n += 1
     n = 3
     for i in range(4):
         Label(window, textvariable='', font=('Helvetica 18'), width=3, height=3, bg="#fff").grid(row=4 + n // 12,column=n % 12)
     for i in alphabet2:
-        Button(window, text=i, command=lambda i=i: Game.if_guess(i), font=('Helvetica 18'), width=5, height=3, bg="#263d42",
+        Button(window, text=i, command=lambda i=i: if_guess(i), font=('Helvetica 18'), width=5, height=3, bg="#263d42",
                fg="white", activebackground="#3e646c", activeforeground="pink").grid(row=4 + n // 12,column=n % 12)
         n += 1
     n = 4
     for i in range(4):
         Label(window, textvariable='', font=('Helvetica 18'), width=3, height=3, bg="#fff").grid(row=5 + n // 10,column=n % 10)
     for i in alphabet3:
-        Button(window, text=i, command=lambda i=i: Game.if_guess(i), font=('Helvetica 18'), width=5, height=3, bg="#263d42",fg="white", activebackground="#3e646c", activeforeground="pink").grid(row=5 + n // 11,column=n % 11)
+        Button(window, text=i, command=lambda i=i: if_guess(i), font=('Helvetica 18'), width=5, height=3, bg="#263d42",fg="white", activebackground="#3e646c", activeforeground="pink").grid(row=5 + n // 11,column=n % 11)
         n += 1
     """
     button for call get_help function and give hint to the user 
     """
-    Button(window, text='hint', command=lambda: Game.get_help(word_for_guess), font=('Helvetica 18'), width=5,
+    Button(window, text='hint', command=lambda: get_help(word_for_guess), font=('Helvetica 18'), width=5,
            height=3, bg="#263d42", fg="white", activebackground="#3e646c", activeforeground="pink").grid(row=5, column=10, sticky='NSWE')
-    Button(window, text='answer as voive', command=lambda i=i: Game.if_guess(get_voice_val()), font=('Helvetica 18'), width=13, height=2, bg="#263d42", fg="white", activebackground="#3e646c", activeforeground="pink").place(relx = 0.387, rely = 0.8815, anchor = CENTER)
-    Game.if_user_want_to_play()
+    Button(window, text='answer as voive', command=lambda i=i: if_guess(get_voice_val()), font=('Helvetica 18'), width=13, height=2, bg="#263d42", fg="white", activebackground="#3e646c", activeforeground="pink").place(relx = 0.387, rely = 0.8815, anchor = CENTER)
+    if_user_want_to_play()
 """
 Category window
 """
@@ -184,8 +178,8 @@ function to check what is the category and call a new function to open new windo
 def what_is_category():
      print(box_value.get())
      if box_value.get():
-        # global category
-        Game.category =box_value.get()
+        global category
+        category =box_value.get()
         game_window()
 box_value=StringVar()
 Label(category_window, text='\nWelcome to Hangman game\n', font=('Helvetica 18'), bg='#fff').pack()
